010067a4545a547f29a46349b7fea9b87994ebf4
Yikuang Chen <bsimchen@gmail.com>
2025-11-05 16:35:46 +0000

    Fix: Add basePath for GitHub Pages deployment

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,13 @@
 import type { NextConfig } from 'next';
 
+const repo = 'LinguaPlayer';
+const assetPrefix = `/${repo}/`;
+const basePath = `/${repo}`;
+
 const nextConfig: NextConfig = {
   output: 'export',
+  assetPrefix: assetPrefix,
+  basePath: basePath,
   /* config options here */
   typescript: {
     ignoreBuildErrors: true,
