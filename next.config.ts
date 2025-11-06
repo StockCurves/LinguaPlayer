ef6b922e264c2628149e8364173a75972b95344c
Yikuang Chen <bsimchen@gmail.com>
2025-11-06 15:16:07 +0000

    github pages的網頁還是舊的 static html, 我要做什麼設定更新嗎？ https://stockcurves.github.

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,7 @@ import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   output: 'export',
+  basePath: '/LinguaPlayer',
   trailingSlash: true,
   /* config options here */
   typescript: {
