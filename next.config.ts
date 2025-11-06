5639df0ef0c2dc58111ea682798887938ac7faa9
Yikuang Chen <bsimchen@gmail.com>
2025-11-06 00:13:20 +0000

    next.js之前設定成export static html, 是否要調整？

--- next.config.ts
+++ next.config.ts
@@ -1,13 +1,7 @@
 import type { NextConfig } from 'next';
 
-const repo = 'LinguaPlayer';
-const assetPrefix = `/${repo}/`;
-const basePath = `/${repo}`;
-
 const nextConfig: NextConfig = {
   output: 'export',
-  assetPrefix: assetPrefix,
-  basePath: basePath,
   trailingSlash: true,
   /* config options here */
   typescript: {
